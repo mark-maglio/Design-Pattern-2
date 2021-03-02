c3f0b8ae56bd84214ec37d33e895ace091ebc478
mark maglio <markmaglio25@gmail.com>
2021-03-02 18:11:26 +0100

    Aggiungere i file di progetto.

---
+++ Design Pattern 2/Sezione.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_2
+{
+    class Sezione
+    {
+        public class Sezione
+        {
+            List<Sezione> sezioni = new List<Sezione>();
+            List<Pagina> pagine = new List<Pagina>();
+
+            internal Sezione() { }
+
+            public string StampaSezione()
+            {
+                string testo = "Sezione\n";
+                foreach (Sezione s in sezioni)
+                {
+                    testo += "- Sezione\n";
+                    foreach (Pagina p in s.pagine)
+                        testo += p.StampaPagina();
+                    testo += "- Fine Sezione\n";
+                }
+                foreach (Pagina p in pagine)
+                    p.StampaPagina();
+                testo += "Fine Sezione\n";
+
+                return testo;
+            }
+        }
+    }
+}
